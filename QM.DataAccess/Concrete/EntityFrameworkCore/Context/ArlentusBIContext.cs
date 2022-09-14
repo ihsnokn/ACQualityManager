9ee62051db215d7563db35b42d84dfb4ea6afe0b
ihsnokn <52039721+ihsnokn@users.noreply.github.com>
2022-09-14 20:25:21 +0300

    Update ArlentusBIContext.cs
    context fix

--- QM.DataAccess/Concrete/EntityFrameworkCore/Context/ArlentusBIContext.cs
+++ QM.DataAccess/Concrete/EntityFrameworkCore/Context/ArlentusBIContext.cs
@@ -12,11 +12,9 @@ namespace QM.DataAccess.Concrete.EntityFrameworkCore.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-P12SOIP\SQLEXPRESS;Database=ArlentusDeneme1;uid=umutd;pwd=Ud4583!");
-            //optionsBuilder.UseSqlServer(@"Server=localhost;Database=ArlentusBIQM;uid=sa;pwd=arlentus");
+            
             optionsBuilder.UseSqlServer(@"Server=localhost;Database=ArlentusBIQM;uid=sa;pwd=arlentus");
-            //optionsBuilder.UseSqlServer(@"Server =94.73.146.3;Database=u0294692_ACBI;uid=u0294692_AC2013;pwd=ArlentusControl2013**);
-            //optionsBuilder.UseSqlServer(@"Server=94.73.170.2;Database=u0406106_ACQMS;uid=u0406106_AC2022;pwd=ArlentusControl2013**");
+            
             base.OnConfiguring(optionsBuilder);
         }
 
